e34d7b73326148dd7755e6b9d2516881ea146938
poornimaramesh <poornimaramesh1995@gmail.com>
2025-02-20 14:37:49 +0300

    add logic for sampling with pps

--- pin_drop_sampling2/utils.py
+++ pin_drop_sampling2/utils.py
@@ -10,6 +10,8 @@ import os
 import requests
 from shapely.ops import nearest_points
 from geopy.distance import geodesic
+from rpy2 import robjects
+
 
 load_dotenv()
 api_key = os.getenv("GOOGLE_MAPS_API_KEY")
@@ -196,3 +198,66 @@ def dist_in_meters(point1, point2):
 
     # Calculate the distance in meters
     return geodesic((lat1, lon1), (lat2, lon2)).meters
+
+
+def sample_locations_with_pps(
+    all_psus: list | np.ndarray,
+    sizes: list | np.ndarray,
+    n_samples: int,
+    with_replacement: bool = False,
+    random_seed: int = 42,    
+) -> tuple:
+    """
+    Sample PSUs with PPS.
+
+    Args:
+        all_psus: List of PSUs from which to sample
+        sizes: Sizes corresponding to each PSU
+        n_samples: Number of samples
+        with_replacement: Whether to sample with replacement
+        random_seed: Random seed
+    """
+    assert len(all_psus) == len(sizes), "Length of all_psus and sizes should be the same"
+    # Get sample sizes for locations
+    r_sizes = robjects.FloatVector(sizes)
+    r_n_samp = robjects.IntVector([n_samples])
+    r_seed = robjects.IntVector([random_seed])
+
+    # Sample locations with PPS using R packages
+    rcode = ""
+    if with_replacement:
+        rcode = str(
+            """
+            function(size, n_samp, seed) {
+                library(TeachingSampling)
+                set.seed(seed)
+                samples <- S.PPS(m=n_samp, x=size)
+                return(samples)
+            }
+            """
+        )
+    else:
+        rcode = str(
+            """
+            function(size, n_samp, seed) {
+                library(TeachingSampling)
+                set.seed(seed)
+                samples <- S.piPS(n=n_samp, x=size)
+                return(samples)
+            }
+            """
+        )
+    r_func = robjects.r(rcode)
+    r_samples = np.array(
+        r_func(size=r_sizes, n_samp=r_n_samp, seed=r_seed)
+    ).T  # <-- transpose so that 1st row = samples; second row = probs
+
+    # Get sampled locations and corresponding shape data
+    sampled_psus_idx = r_samples[0].astype(int) - 1  # <-- R is 1-indexed
+    pps_sampled_psus = all_psus[sampled_psus_idx]
+
+    return (
+        pps_sampled_psus,
+        sampled_psus_idx,
+        r_samples[1],
+    )
